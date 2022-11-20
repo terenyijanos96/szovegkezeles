becb6ae7d576ca89f3c91b19414a7c749ee192e8
Terényi János <terenyi.janos96@gmail.com>
2022-11-20 21:26:19 +0100

    nevbekero, kor_ado és legidosebb_ember_kereso függvények hozzáadása

--- fuggvenyek.py
+++ fuggvenyek.py
@@ -1,3 +1,5 @@
+import random
+
 szoveg = ""
 
 
@@ -8,22 +10,23 @@ def nagybetus():
 
 
 def hosszabb_mint10():
-    kimenet = False
-
     if len(szoveg) > 10:
-        kimenet = len(szoveg)
+        kimenet = f"Hosszabb! (Összesen {len(szoveg)} karakter hosszú)"
+    else:
+        kimenet = "Nem hosszabb."
 
     return kimenet
 
 
 def betu3():
-
+    global szoveg
     szoveg = input("Kérek egy legalább 3-betűs szöveget!:\n>>")
     while len(szoveg) < 3:
         szoveg = input("HIBA! Túl rövid a szöveg! Legalább 3 karakternek kell lennie!:\n>>")
 
 
 def elso_a():
+    global szoveg
     szoveg = input('Kérek egy szöveget és megkeresem az első "a"-betűt!:\n>>')
     i = 0
     elso_index = None
@@ -39,7 +42,6 @@ def elso_a():
 
 
 def feketeoves():
-
     i = 0
     betu_lista = []
     while i < len(szoveg):
@@ -61,3 +63,53 @@ def feketeoves():
         i += 1
 
     return kimenet
+
+
+nevlista = []
+
+
+def nev_bekero():
+    global nevlista
+    fut = True
+    print("Kérek adjon meg neveket és azokat nagy kezdőbetűssé alakítom!\n"
+          "Kilépéshez adjon meg egy @-jelet (kukac jel)!")
+
+    while fut:
+        nev = input(">> ")
+
+        if nev != "@":
+            nevlista.append(nev.capitalize())
+        else:
+            fut = False
+
+    return nevlista
+
+
+kor = []
+
+
+def kor_ado():
+    global kor
+
+    i = 0
+
+    while i < len(nevlista):
+        rand = int(random.random() * 100) + 1
+        kor.append(rand)
+        i += 1
+
+    return kor
+
+
+def legidosebb_ember_kereso():
+    maximum = kor[0]
+    max_index = 0
+    i = 1
+
+    while i < len(kor):
+        if maximum < kor[i]:
+            maximum = kor[i]
+            max_index = i
+        i += 1
+
+    return nevlista[max_index], maximum
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from fuggvenyek import *
+
 nagybetu = nagybetus()
 print(f"Adott szöveg nagybetűsen: {nagybetu}\n")
 hosszabb = hosszabb_mint10()
-print(f"Hosszabb-e mint 10 karakter? Ha igen, akkor hány karakteres? {hosszabb}\n")
+print(f"Hosszabb-e mint 10 karakter? {hosszabb}\n")
 
 betu3()
 print()
@@ -13,3 +14,10 @@ print(f'Összesen ennyi "a" betű van a szövegben: {hany_a}\n')
 
 nehez = feketeoves()
 print(nehez)
+
+# nev, kor = nevbekero(), kor_ado()
+# print(nev)
+# print(kor)
+
+nev, kor = legidosebb_ember_kereso()
+print(f"A legidősebb: {nev}, aki {kor} éves")
